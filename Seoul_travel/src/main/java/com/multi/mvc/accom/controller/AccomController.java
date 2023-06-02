e8d8fe204886a9bcc065c1944bf5fbf512b1e447
yunjeongLim <630yjl@gmail.com>
2023-06-02 18:21:26 +0900

    숙소 컨트롤러 수정 / 경로 연결

--- Seoul_travel/src/main/java/com/multi/mvc/accom/controller/AccomController.java
+++ Seoul_travel/src/main/java/com/multi/mvc/accom/controller/AccomController.java
@@ -42,7 +42,6 @@ public class AccomController {
 		return "4_accommodation";
 	}
 	
-	
 	@RequestMapping("/accomDetail")
 	public String accomMain(Model model, int id) {
 		Accommodation accom = accomoService.selectByContentId(id);
@@ -52,6 +51,7 @@ public class AccomController {
 		model.addAttribute("item", accom);
 		return "4.2_accommoInfo";	
 	}
+	
 	
 	
 	@RequestMapping("/accomGall")
@@ -76,6 +76,20 @@ public class AccomController {
 		
 		return "4.2_accommoReview";	
 	}
+	
+	
+	
+	@RequestMapping("/accomCate")
+	public String accomCate() {
+//		Accommodation accomCate = accomoService.selectByContentId(id);
+//		if(accomCate == null) {
+//			 에러처리
+//		}
+//		model.addAttribute("item2", accomReiview);
+		
+		return "4.1_accommodationCategory";	
+	}
+	
 	
 
 }
